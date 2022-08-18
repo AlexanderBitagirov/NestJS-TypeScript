9d4a61bd14e3a0f02943260810bb6a2fff15e672
DESKTOP-MJQAUR8\alexa <bitagirov@gmail.com>
2022-08-18 10:36:54 +0300

    added arrow to button

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ const Home: NextPage = () => {
   return (
     <>
       <Htag tag="h1">Text</Htag>
-      <Button appearance='ghost'> Button 1</Button>
-      <Button appearance='primary'> Button 2</Button>
+      <Button appearance='ghost' arrow='down'> Button 1</Button>
+      <Button appearance='primary' arrow='right'> Button 2</Button>
     </>
   );
 };
